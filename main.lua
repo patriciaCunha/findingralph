c9221518441e3a10daca3e99ee7e939808680e15
patriciaCunha <vippatriciac@gmail.com>
2019-03-18 20:13:37 -0300

    inclusão de plataformas para que a personagem possa pular sobre elas

--- main.lua
+++ main.lua
@@ -18,20 +18,32 @@ ground.x = 160
 ground.y = 490
 physics.addBody(ground , "static")
 
-local player = display.newImage("player.png", 360, 125)
-player.x = 175
-player.y = 300
-physics.addBody(player)
+local platform = display.newImage( "platform1.png", 100, 50 )
+platform.x = 250
+platform.y = 250
+physics.addBody(platform , "static")
+
+local platform2 = display.newImage( "platform2.png", 100, 50 )
+platform2.x = -1
+platform2.y = 100
+physics.addBody(platform2 , "static")
+
+local player = display.newImageRect("player.png", -100, 70)
+player.x = 100
+player.y = 340
 
 local function onTouch(event)
     if(event.phase == "began") then
+-- player.y = 300
+physics.addBody(player)
         if(event.x <player.x) then
             -- jump left
-            player:setLinearVelocity(-30, -200)
+            player:setLinearVelocity(-30, -150)
         else
             -- jump right
-            player:setLinearVelocity(30, -200)
+            player:setLinearVelocity(30, -150)
         end    
     end
+    return true
 end
 Runtime:addEventListener("touch", onTouch)        
